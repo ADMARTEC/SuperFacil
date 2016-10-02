a23f10b2627f6cdc2d4d84abf265ae6ca864ca4c
Martinho Andre Sebastiao <martinho20@live.com.pt>
2016-10-02 13:13:16 +0100

    Organização de projecto

---
+++ SuperFacil/SuperFacil.Dominio/Modelos/Administração/Adm_Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFacil.Dominio.Modelos.Administração
+{
+    public class Adm_Usuario
+    {
+    }
+}
