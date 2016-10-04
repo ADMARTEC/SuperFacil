c0c49350db2956ca713e12b0f97814d38714b0cb
Paulo Alexandre <paulada@live.com.pt>
2016-10-04 11:54:07 -0300

    Adição de mais propriedades

--- SuperFacil/SuperFacil.Dominio/Modelos/Abstracao/Abs_Pessoa.cs
+++ SuperFacil/SuperFacil.Dominio/Modelos/Abstracao/Abs_Pessoa.cs
@@ -6,5 +6,12 @@ namespace SuperFacil.Dominio.Modelos.Abstracao
     {
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
+        public String Biografia { get; set; }
+        public String Email { get; set; } 
+        public int Pais { get; set; } // País, Coloquei Int na ideia de poder receber o numero ref. do país 
+        public String Telefone { get; set; }
+        public String Notas { get; set; }
+        public String Cidade { get; set; }
+        
     }
 }
