cfbbcaaa7ea078a829d759ca939412f247cbd9a7
Adilson Lukeny da Silva Pedro <adilson.lukeny@hotmail.com>
2016-10-16 20:29:33 +0100

    Parte InfraEstrutura--- Adicçao das classes: Mul_Midia_Repositorio, Mul_Autor_Repositorio.. Parte Dominio--- Adicção das classes: IMul_Autor_Repositorio, IMul_Autor_Servico e IMul_Biografia_Repositorio, IMul_Biografia_Servico.
    Att: Dominio.Modelos.Multimedia faltam métodos as classes: Mul_Biografia, Mul_Autor, Mul_Localizacao

--- SuperFacil/SuperFacil.Dominio/Contratos/Repositorios/Multimidia/IMul_Midia_Repositorio.cs
+++ SuperFacil/SuperFacil.Dominio/Contratos/Repositorios/Multimidia/IMul_Midia_Repositorio.cs
@@ -6,14 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SuperFacil.Dominio.Contratos.Repositorios.Multimidia
-{
-    public interface IMul_Midia_Repositorio : IDisposable
     {
+    public interface IMul_Midia_Repositorio : IDisposable
+        {
 
         #region SELECT - R
         Task<Mul_Midia> GetByID(int Empresa, int value);
         Task<Mul_Midia> GetByParent(int Empresa, int value);
-        Task<Mul_Midia> GetByFixa(int Empresa, int value);
+        Task<Mul_Midia> GetByFaixa(int Empresa, int value);
         Task<Mul_Midia> GetByDesiginacao(int Empresa, string value);
         Task<Mul_Midia> GetByAutor(int Empresa, int value);
         Task<Mul_Midia> GetByTag(int Empresa, string value);
@@ -26,5 +26,5 @@ namespace SuperFacil.Dominio.Contratos.Repositorios.Multimidia
         void Update(Mul_Midia midia);
         void Deleted(Mul_Midia midia);
         #endregion
+        }
     }
-}
