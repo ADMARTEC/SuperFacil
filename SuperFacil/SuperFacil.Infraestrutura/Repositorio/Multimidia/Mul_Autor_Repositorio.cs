212b260a2c15e278a0f823b5c9dcbb919e4a695a
Martinho Andre Sebastiao <martinho20@live.com.pt>
2016-10-17 18:14:16 +0100

    Identação

--- SuperFacil/SuperFacil.Infraestrutura/Repositorio/Multimidia/Mul_Autor_Repositorio.cs
+++ SuperFacil/SuperFacil.Infraestrutura/Repositorio/Multimidia/Mul_Autor_Repositorio.cs
@@ -7,37 +7,37 @@ using System.Threading.Tasks;
 using SuperFacil.Dominio.Modelos.Multimidia;
 
 namespace SuperFacil.Infraestrutura.Repositorio.Multimidia
-    {
+{
     public class Mul_Autor_Repositorio : IMul_Autor_Repositorio
-        {
+    {
         public void Create(Mul_Autor autor)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public void Deleted(Mul_Autor autor)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public void Dispose()
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Autor> GetByDesiginacao(int Empresa, string value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Autor> GetByID(int Empresa, int value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public void Update(Mul_Autor autor)
-            {
+        {
             throw new NotImplementedException();
-            }
         }
     }
+}
--- SuperFacil/SuperFacil.Infraestrutura/Repositorio/Multimidia/Mul_Midia_Repositorio.cs
+++ SuperFacil/SuperFacil.Infraestrutura/Repositorio/Multimidia/Mul_Midia_Repositorio.cs
@@ -7,68 +7,67 @@ using System.Threading.Tasks;
 using SuperFacil.Dominio.Modelos.Multimidia;
 
 namespace SuperFacil.Infraestrutura.Repositorio.Multimidia
-    {
+{
     public class Mul_Midia_Repositorio : IMul_Midia_Repositorio
-
-        {
+    {
         public void Create(Mul_Midia midia)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public void Deleted(Mul_Midia midia)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public void Dispose()
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Midia> GetByAno_Lancamento(int Empresa, DateTime value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Midia> GetByAutor(int Empresa, int value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Midia> GetByDesiginacao(int Empresa, string value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Midia> GetByFaixa(int Empresa, int value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Midia> GetByID(int Empresa, int value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Midia> GetByLocalizacao(int Empresa, int value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Midia> GetByParent(int Empresa, int value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public Task<Mul_Midia> GetByTag(int Empresa, string value)
-            {
+        {
             throw new NotImplementedException();
-            }
+        }
 
         public void Update(Mul_Midia midia)
-            {
+        {
             throw new NotImplementedException();
-            }
         }
     }
+}
