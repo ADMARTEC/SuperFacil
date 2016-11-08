daf96ec2f574629f348e50c4f5331a5fc062ffa8
Adilson Lukeny da Silva Pedro <adilson.lukeny@hotmail.com>
2016-11-08 20:11:56 +0100

    Adicção das Classes: Mul_Midia_Sevico e Mul_Autor_Servco e correção da classe Mul_Midia

---
+++ SuperFacil/SuperFacil.Bussines/Servicos/Multimidia/Mul_Midia_Servico.cs
@@ -0,0 +1,56 @@
+﻿using SuperFacil.Dominio.Contratos.Servicos.Multimidia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperFacil.Dominio.Modelos.Multimidia;
+using SuperFacil.Dominio.Contratos.Repositorios.Multimidia;
+
+namespace SuperFacil.Bussines.Servicos.Multimidia
+    {
+    public class Mul_Midia_Servico : IMul_Midia_Servico
+        {
+
+        private readonly IMul_Midia_Repositorio _repositorio;
+
+        public Mul_Midia_Servico() { }
+
+        public Mul_Midia_Servico(IMul_Midia_Repositorio repositorio)
+            {
+            _repositorio = repositorio;
+            }
+        public void Dispose()
+            {
+                {
+                _repositorio.Dispose();
+                GC.Collect();
+                }
+            }
+
+        public int GetAutor(int Empresa, int value)
+            {
+            throw new NotImplementedException();
+            }
+
+        public Mul_Midia GetByDesignacao(int Empresa, string value)
+            {
+            throw new NotImplementedException();
+            }
+
+        public Mul_Midia GetByMidia_ID(int Empresa, int value)
+            {
+            throw new NotImplementedException();
+            }
+
+        public int GetLocalizacao(int Empresa, int value)
+            {
+            throw new NotImplementedException();
+            }
+
+        public void Registrar(Mul_Midia midia)
+            {
+            throw new NotImplementedException();
+            }
+        }
+    }
