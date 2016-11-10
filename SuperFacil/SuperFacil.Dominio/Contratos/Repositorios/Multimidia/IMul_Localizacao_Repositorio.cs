b78297f32a2e195b05e658c3e67dbeac7adc4dfb
Adilson Lukeny da Silva Pedro <adilson.lukeny@hotmail.com>
2016-11-10 21:28:21 +0100

    Adicção da Classe: IMul_Localizacao_Repositorio...

---
+++ SuperFacil/SuperFacil.Dominio/Contratos/Repositorios/Multimidia/IMul_Localizacao_Repositorio.cs
@@ -0,0 +1,22 @@
+﻿using SuperFacil.Dominio.Modelos.Multimidia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFacil.Dominio.Contratos.Repositorios.Multimidia
+    {
+    public interface IMul_Localizacao_Repositorio : IDisposable
+        {
+        #region SELECT - R
+        Task<Mul_Localizacao> GetByID(int Empresa, int value);
+        #endregion
+
+        #region CUD
+        void Create(Mul_Localizacao localizacao);
+        void Update(Mul_Localizacao localizacao);
+        void Deleted(Mul_Localizacao localizacao);
+        #endregion
+        }
+    }
--- SuperFacil/SuperFacil.Infraestrutura/Repositorio/Multimidia/Mul_Localizacao_Repositorio.cs
+++ SuperFacil/SuperFacil.Infraestrutura/Repositorio/Multimidia/Mul_Localizacao_Repositorio.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using SuperFacil.Dominio.Contratos.Repositorios.Multimidia;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SuperFacil.Dominio.Modelos.Multimidia;
+using System.Data.Entity;
 
 namespace SuperFacil.Infraestrutura.Repositorio.Multimidia
     {
-    class Mul_Localizacao_Repositorio
+    public class Mul_Localizacao_Repositorio : IMul_Localizacao_Repositorio
         {
+        private SuperFacilContexto db;
+        public Mul_Localizacao_Repositorio(SuperFacilContexto _db)
+            {
+            db = _db;
+            }
+
+        public async void Create(Mul_Localizacao localizacao)
+            {
+            db.Mul_Localizacao.Add(localizacao);
+            await db.SaveChangesAsync();
+            }
+
+        public void Deleted(Mul_Localizacao localizacao)
+            {
+            Update(localizacao);
+            }
+
+        public void Dispose()
+            {
+            db.Dispose();
+            GC.Collect();
+            }
+
+        public Task<Mul_Localizacao> GetByID(int Empresa, int value)
+            {
+            return db.Mul_Localizacao.Where(x => x.Empresa_ID == Empresa && x.Localizacao_ID == value).FirstOrDefaultAsync();
+            }
+
+        public async void Update(Mul_Localizacao localizacao)
+            {
+            db.Entry<Mul_Localizacao>(localizacao).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            }
         }
     }
